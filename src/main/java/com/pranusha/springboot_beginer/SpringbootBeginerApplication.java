dfd36b0739119ea8844578fb0a187d27d78ab805
pranusha <pranushareddy925@gmail.com>
2026-06-15 23:02:22 +0530

    Integreated html and css

---
+++ src/main/java/com/pranusha/springboot_beginer/SpringbootBeginerApplication.java
@@ -0,0 +1,13 @@
+package com.pranusha.springboot_beginer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBeginerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBeginerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pranusha/springboot_beginer/SpringbootBeginerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranusha.springboot_beginer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBeginerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
